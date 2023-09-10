ddcda012007e4317db6eac696494b709c2b38000
uri752 <uri752blag@mail.ru>
2023-09-10 17:16:58 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from './components/Card/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="container">
+      <Card>
+        <div className="card" style={{width: 18 + 'rem'}}>        
+          <img src='https://sobakovod.club/uploads/posts/2021-12/thumbs/1639036549_5-sobakovod-club-p-sobaki-sobaka-na-otdikhe-5.jpg' 
+              className="card-img-top" alt='Image cap'/>
+          <div className="card-body">
+            <h5 className="card-title">Карточка с картинкой</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href='#' className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </Card>
+
+      <Card>
+        <div className="card" style={{width: 18 + 'rem'}}>                
+          <div className="card-body">
+            <h5 className="card-title">Карточка без картинки</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href='#' className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </Card>
+
+   </div>
   );
 }
 
